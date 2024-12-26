856e199e07e160b0c573f7259d0c1a97af5aee83
Lolshh <gasthoral@gmail.com>
2024-12-26 16:48:40 +0100

    Simplified workflow. Removed refining automation for now. Removed tools for now. Removed conditional edges used to choose whether to go to the next step of to continue the current process by a while loop.

--- app.py
+++ app.py
@@ -5,21 +5,16 @@ from rich.console import Console # type: ignore
 from rich.markdown import Markdown # type: ignore
 from rich.prompt import Confirm # type: ignore
 
-
 from langchain_openai import ChatOpenAI # type: ignore
 from langchain_core.messages import HumanMessage, SystemMessage # type: ignore
 
-
-from typing import Annotated
+from typing import Annotated, List, Optional
 
 from typing_extensions import TypedDict # type: ignore
 
 from langgraph.graph import StateGraph, START, END # type: ignore
 from langgraph.graph.message import add_messages # type: ignore
 from langgraph.checkpoint.memory import MemorySaver # type: ignore
-from langgraph.prebuilt import ToolNode # type: ignore
-from langchain_core.tools import tool # type: ignore
-
 
 from IPython.display import Image, display # type: ignore
 from langchain_core.runnables.graph import CurveStyle, MermaidDrawMethod, NodeStyles # type: ignore
@@ -27,49 +22,50 @@ from langchain_core.runnables.graph import CurveStyle, MermaidDrawMethod, NodeSt
 import io
 from PIL import Image # type: ignore
 
-class State(TypedDict):
+from datetime import date as Date
+
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
+
+DEFAULT_MODE = 'human'
+DEFAULT_CONFIG = {"configurable": {"thread_id": "1"}}
+
+class SearchQuery(TypedDict):
+    query: str
+    queries: Optional[List[str]]
+    keywords: Optional[List[str]]
+    start_date: Optional[Date]
+    end_date: Optional[Date]
+
+class Paper(TypedDict):
+    title: str
+    summary: str
+    link: Optional[str]
+    date: Optional[Date]
+    authors: Optional[List[str]]
+
+class ArxivPaper(TypedDict):
+    paper: Paper
+    keywords: Optional[List[str]]
+    sources: Optional[List[Paper]]
+
+class RefineAgentState(TypedDict):
     messages: Annotated[list, add_messages]
     mode: str
     done: bool
+    iterations: int
 
-def refine_auto(state: State, llm: ChatOpenAI):
-    #TODO
-    return state
+class SearchAgentState(TypedDict):
+    messages: Annotated[list, add_messages]
+    search_query: Optional[SearchQuery]
+    papers: Optional[List[ArxivPaper]]
+    done: bool
 
-def route_refine(state: State, llm: ChatOpenAI):
-    routing_prompt = """
-    You are assisting with a research topic. Your task is to determine whether the user’s query has been well understood and whether there is an agreement between the user and the assistant. Specifically, you should:
+class State(TypedDict):
+    refine_agent: RefineAgentState
+    search_agent: SearchAgentState
 
-    1. Analyze the user's query and identify the key topic or question being asked.
-    2. Check whether the assistant has correctly captured the main points of the query.
-    3. Confirm if the user has expressed clear expectations regarding the research direction or question. 
-    4. Respond with an assessment of whether the research topic is understood and agreed upon.
-
-    If the query has nothing to do with research, return "refine".
-    If the user query is clear and the assistant’s response aligns with the query, respond with "done". 
-    If there is a lack of clarity or ambiguity in the user's query, or if the assistant’s response does not align with the query, respond with "refine."
-    """
-    routing_message = SystemMessage(routing_prompt)
-
-    response = llm.invoke(state["messages"] + [routing_message])
-    
-    if not len(state["messages"]):
-        return "chatbot"
-    
-    if state["mode"] == "human":
-        if "done" in response.content.lower():
-            return "terminate_refine"
-        else:
-            return "chatbot"
-    elif state["mode"] == "auto":
-        if "done" in response.content.lower():
-            return "terminate_refine"
-        else:
-            return "refine_auto"
-    else:
-        raise ValueError("Invalid mode for Agent State")    
-
-def chatbot(state: State, llm: ChatOpenAI, console: Console):
+def refine_agent(state: State, llm: ChatOpenAI, console: Console):
+    print("RefineAgent")
     system_prompt = """
     If the query is not about a research topic ignore the following.
     You are a research assistant helping the user refine their research query. Your task is to identify areas of ambiguity or insufficient detail in the user's query and ask clarifying questions to help them focus their research. Here's how you should approach it:
@@ -82,104 +78,106 @@ def chatbot(state: State, llm: ChatOpenAI, console: Console):
     - Any other missing information that could help define the research direction more clearly.
     3. Ask open-ended questions to prompt the user to provide more specific details.
     4. Make sure to keep the user's original research intent intact while helping them narrow down their focus.
+
+    Your goal is not to find papers. Only to refine the query with the user.
+
+    If you feel that the refinement process is over or that the user has nothing else to add, simply return 'done'.
     """
 
     system_message = SystemMessage(system_prompt)
+    while True:
 
-
-    user_input = console.input("[bold blue]Ask your question (type 'exit' to quit): [/bold blue]")
-    
-    if user_input.lower() == "exit":
-        console.print("[bold green]Goodbye![/bold green]")
-    else:
-        try:
-            user_message = HumanMessage(content=user_input)
-            state["messages"].append(user_message)
-            
-            response = llm.invoke(state["messages"] + [system_message])
-            response_message = response.content
-            state["messages"].append(response)
-            console.print(Markdown(response_message))
+        user_input = console.input("[bold blue]Ask your question (type 'exit' to quit): [/bold blue]")
         
-        except Exception as e:
-            console.print(f"[bold red]An error occurred: {e}[/bold red]")
+        if user_input.lower() == "exit":
+            console.print("[bold green]Goodbye![/bold green]")
+        else:
+            try:
+                user_message = HumanMessage(content=user_input)
+                state["refine_agent"]["messages"].append(user_message)
+
+                if state["refine_agent"]["iterations"] == 3:
+                    if Confirm.ask("\n[yellow]End query refinement?[/yellow]"):
+                        new_state = RefineAgentState(
+                            {
+                                "messages": state["refine_agent"]["messages"],
+                                "mode": state["refine_agent"]["mode"],
+                                "done": True,
+                                "iterations": 0,
+                            }
+                        )
+                        return {"refine_agent": new_state, "search_agent": state["search_agent"]}
+                    else:
+                        state["refine_agent"]["iterations"] = 0
+                
+                response = llm.invoke(state["refine_agent"]["messages"] + [system_message])
+                response_message = response.content
+
+                if "done" in response_message.lower():
+                    new_state = RefineAgentState(
+                        {
+                            "messages": state["refine_agent"]["messages"],
+                            "mode": state["refine_agent"]["mode"],
+                            "done": True,
+                            "iterations": 0,
+                        }
+                    )
+                    return {"refine_agent": new_state, "search_agent": state["search_agent"]}
+                
+                state["refine_agent"]["messages"].append(response)
+
+                console.print(Markdown(response_message))
+
+                state["refine_agent"]["iterations"] += 1
+            
+            except Exception as e:
+                console.print(f"[bold red]An error occurred: {e}[/bold red]")
+
+def build_query(state: State, llm: ChatOpenAI) -> State:
+    #TODO
+    print("BuildQueryAgent")
+
+    query = SearchQuery(
+        query="Test Query",
+        summary="This is a test query used during development."
+    )
+
+    state["search_agent"]["search_query"] = query
+
     return state
 
-def terminate_refine(state: State, llm: ChatOpenAI):
+def search_agent(state: State, llm: ChatOpenAI):
     #TODO
+    print("SearchAgent")
+    print(state["search_agent"])
     return state
 
-def search(state: State, llm: ChatOpenAI):
+def build_answer(state: State, llm: ChatOpenAI):
     #TODO
+    print("BuildAnswerAgent")
     return state
 
-@tool
-def arxiv_search():
-    #TODO
-    """
-    Tool used to browse arXiv.
-    """
-    return
-
-
-def route_search(state: State, llm: ChatOpenAI):
-    #TODO
-    return "terminate_search"
-
-def terminate_search(state: State, llm: ChatOpenAI):
-    #TODO
-    return state
-
-def build_graph(llm: ChatOpenAI, memory: MemorySaver, tools, console: Console):
+def build_graph(llm: ChatOpenAI, memory: MemorySaver, console: Console):
     workflow = StateGraph(State)
-    
-    workflow.add_node("route_refine", lambda s: s)
-    workflow.add_node("refine_auto", lambda s: refine_auto(s, llm))
-    workflow.add_node("chatbot", lambda s: chatbot(s, llm, console))
-    workflow.add_node("terminate_refine", lambda s: terminate_refine(s, llm))
 
-    workflow.add_node("route_search", lambda s: s)
-    workflow.add_node("search", lambda s: search(s, llm))
+    workflow.add_node("RefineAgent", lambda s: refine_agent(s, llm, console))
+    workflow.add_node("BuildQueryAgent", lambda s: build_query(s, llm))
 
-    workflow.add_node("terminate_search", lambda s: terminate_search(s, llm))
+    workflow.add_node("SearchAgent", lambda s: search_agent(s, llm))
 
-    tool_node = ToolNode(tools)
-    workflow.add_node("tools", tool_node)
+    workflow.add_node("BuildAnswerAgent", lambda s: build_answer(s, llm))
 
+    workflow.add_edge(START, "RefineAgent")
 
-    workflow.add_edge(START, "route_refine")
+    workflow.add_edge("RefineAgent", "BuildQueryAgent")
 
-    workflow.add_conditional_edges(
-        "route_refine",
-        lambda s: route_refine(s, llm),
-        {
-            "chatbot": "chatbot",
-            "refine_auto": "refine_auto",
-            "terminate_refine": "terminate_refine",
-        }
-    )
+    workflow.add_edge("BuildQueryAgent", "SearchAgent")
 
-    workflow.add_edge("chatbot", "route_refine")
-    workflow.add_edge("refine_auto", "route_refine")
-    workflow.add_edge("terminate_refine", "search")
+    workflow.add_edge("SearchAgent", "BuildAnswerAgent")
 
-    workflow.add_conditional_edges(
-        "route_search",
-        lambda s: route_refine(s, llm),
-        {
-            "search": "search",
-            "tools": "tools",
-            "terminate_search": "terminate_search",
-        }
-    )
-
-    workflow.add_edge("search", "route_search")
-    workflow.add_edge("tools", "search")
-
-    workflow.add_edge("terminate_search", END)
+    workflow.add_edge("BuildAnswerAgent", END)
 
     return workflow.compile(checkpointer=memory)
-
     
 def show_graph(workflow, console: Console):
     try:
@@ -194,34 +192,60 @@ def show_graph(workflow, console: Console):
     except Exception as e:
         console.print(f"Encountered error {e}.")
     
+def initialize_agent_state(mode: str):
+    refine_agent_state = RefineAgentState(
+        {
+            "messages": [],
+            "mode": mode,
+            "done": False,
+            "iterations": 0,
+        }
+    )
 
+    search_agent_state = SearchAgentState(
+        {
+            "messages": [],
+            "search_query": None,
+            "papers": None,
+            "done": False,
+        }
+    )
+
+    agent_state = State(
+        {
+            "refine_agent": refine_agent_state,
+            "search_agent": search_agent_state,
+        }
+    )
+
+    return agent_state
 def main():
     load_dotenv()
     
     console = Console()
 
-    OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
-    tools = [arxiv_search]
     llm = ChatOpenAI(model=OPENAI_MODEL)
 
-    mode = console.input("[bold blue]Do you want to work alongside the LLM during its research? (type 'auto' for the LLM to work alone, otherwise type 'human'): [/bold blue]")
-    while mode not in ["auto", "human"]:
-        console.print("[bold red]Invalid input. Please try again.[/bold red]")
-        mode = console.input("[bold blue]Do you want to work alongside the LLM during its research? (type 'auto' for the LLM to work alone, otherwise type 'human'): [/bold blue]")
+    # mode = console.input("[bold blue]Do you want to work alongside the LLM during its research? (type 'auto' for the LLM to work alone, otherwise type 'human'): [/bold blue]")
+    # while mode not in ["auto", "human"]:
+    #     console.print("[bold red]Invalid input. Please try again.[/bold red]")
+    #     mode = console.input("[bold blue]Do you want to work alongside the LLM during its research? (type 'auto' for the LLM to work alone, otherwise type 'human'): [/bold blue]")
     
-    workflow = build_graph(llm, MemorySaver(), tools, console)
+    # TODO: For now the mode is 'human' by defeault
+    mode = DEFAULT_MODE
 
-    config = {"configurable": {"thread_id": "1"}}
+    workflow = build_graph(llm, MemorySaver(), console)
 
-    state = {"messages": [], "mode": mode}
+    config = DEFAULT_CONFIG
+
+    state = initialize_agent_state(mode)
+    
     if Confirm.ask("\n[yellow]Display the graph?[/yellow]"):
         show_graph(workflow, console)
-    
     try: 
         workflow.invoke(state, config, stream_mode="values")
     except Exception as e:
         console.print(f"[bold red]An error occurred: {e}[/bold red]")
-    
     return
 
 if __name__ == "__main__":
